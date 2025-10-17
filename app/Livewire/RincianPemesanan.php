2187a526b454d8fa10aeb0f6ab908602df438b1c
Rafa Aliftha <rafaaliftha@gmail.com>
2025-10-17 16:37:15 +0700

    file kosong silakan diisi

---
+++ app/Livewire/RincianPemesanan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RincianPemesanan extends Component
+{
+    public function render()
+    {
+        return view('livewire.rincian-pemesanan');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Livewire\Login;
+use App\Livewire\Loading;
+use App\Livewire\Navbar;
+use App\Livewire\Beranda;
+use App\Livewire\RincianTiket;
+use App\Livewire\RincianPemesanan;
+use App\Livewire\ETiket;
+use App\Livewire\Profil;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', Login::class)->name('login');
